5f1dcb01776b5408413a872f3b7ad41ceae6e9b3
chloebaisse1 <baissechloe@gmail.com>
2025-05-05 17:24:33 +0400

    inscripiton/connexion avec Clerck

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignUp />
+    </div>
+  );
+};
+
+export default page;
