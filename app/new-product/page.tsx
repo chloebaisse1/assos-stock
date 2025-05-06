5433a429caad8621c450b3ac92d1839196807d01
chloebaisse1 <baissechloe@gmail.com>
2025-05-06 16:07:31 +0400

    formulaire creation de produit

--- app/new-product/page.tsx
+++ app/new-product/page.tsx
@@ -1,8 +1,140 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
+import { useEffect, useState } from "react";
 import Wrapper from "../components/Wrapper";
+import { useUser } from "@clerk/nextjs";
+import { Category } from "@prisma/client";
+import { FormDataType } from "@/type";
+import { readCategories } from "../actions";
 
 const page = () => {
-  return <Wrapper>test</Wrapper>;
+  const { user } = useUser();
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+
+  const [file, setFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [formData, setFormData] = useState<FormDataType>({
+    name: "",
+    description: "",
+    price: 0,
+    categoryId: "",
+    unit: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        if (email) {
+          const data = await readCategories(email);
+          if (data) {
+            setCategories(data);
+          }
+        }
+      } catch (error) {
+        console.error("Erreur lors du chargement des catégories", error);
+      }
+    };
+    fetchCategories();
+  }, [email]);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] || null;
+    setFile(selectedFile);
+    if (selectedFile) {
+      setPreviewUrl(URL.createObjectURL(selectedFile));
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div className="flex justify-between items-center">
+        <div className="text-2xl font-bold mb-4">
+          <h1 className="p-3">Créer un produit</h1>
+          <section className="flex md:flex-row flex-col">
+            <div className="space-y-4 md:w-[450px]">
+              <input
+                type="text"
+                name="name"
+                placeholder="Nom"
+                className="input input-bordered w-full"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <textarea
+                name="description"
+                id="description"
+                placeholder="Description"
+                className="textarea textarea-bordered w-full"
+                value={formData.description}
+                onChange={handleChange}
+              ></textarea>
+
+              <input
+                type="number"
+                name="price"
+                placeholder="Prix"
+                className="input input-bordered w-full"
+                value={formData.price}
+                onChange={handleChange}
+              />
+
+              <select
+                className="select select-bordered w-full"
+                value={formData.categoryId}
+                onChange={handleChange}
+                name="categoryId"
+              >
+                <option value="">Séléctionner une catégorie</option>
+                {categories.map((cat) => (
+                  <option key={cat.id} value={cat.id}>
+                    {cat.name}
+                  </option>
+                ))}
+              </select>
+
+              <select
+                className="select select-bordered w-full"
+                value={formData.unit}
+                onChange={handleChange}
+                name="unit"
+              >
+                <option value="">Séléctionner l'unité</option>
+                <option value="g">Gramme</option>
+                <option value="kg">Kilogramme</option>
+                <option value="l">Litre</option>
+                <option value="m">Mètre</option>
+                <option value="cm">Centimètre</option>
+                <option value="h">Heure</option>
+                <option value="pcs">Pièces</option>
+              </select>
+
+              <input
+                type="file"
+                accept="image/*"
+                placeholder="Image du produit"
+                className="file-input file-input-bordered w-full"
+                onChange={handleFileChange}
+              />
+
+              <button className="btn btn-secondary">Créer le produit</button>
+            </div>
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default page;
